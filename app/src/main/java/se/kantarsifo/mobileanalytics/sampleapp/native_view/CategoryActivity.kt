6d3bb4449a7ee38837dc0aad96e47d85f5b1dc92
MetiZ1 <80488226+MetiZ1@users.noreply.github.com>
2025-09-16 11:14:33 +0200

    updated kotlin and gradle to get support from latest android studio (#13)
    * updated kotlin and gradle to get support from latest android studio
    
    * upgraded agp to 8.7.2
    
    * updated min sdk version and all libraries

--- app/src/main/java/se/kantarsifo/mobileanalytics/sampleapp/native_view/CategoryActivity.kt
+++ app/src/main/java/se/kantarsifo/mobileanalytics/sampleapp/native_view/CategoryActivity.kt
@@ -3,17 +3,19 @@ package se.kantarsifo.mobileanalytics.sampleapp.native_view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_category.*
 import se.kantarsifo.mobileanalytics.framework.TSMobileAnalytics
-import se.kantarsifo.mobileanalytics.sampleapp.R
 import se.kantarsifo.mobileanalytics.sampleapp.base.BaseActivity
+import se.kantarsifo.mobileanalytics.sampleapp.databinding.ActivityCategoryBinding
 import se.kantarsifo.mobileanalytics.sampleapp.util.TagInfo
 
 class CategoryActivity : BaseActivity() {
 
+    private lateinit var binding: ActivityCategoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category)
+        binding = ActivityCategoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         init()
     }
 
@@ -25,7 +27,7 @@ class CategoryActivity : BaseActivity() {
 
     private fun init() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        list.setOnItemClickListener { parent, _, position, _ ->
+        binding.list.setOnItemClickListener { parent, _, position, _ ->
             sendTag(parent.getItemAtPosition(position) as String)
         }
     }
